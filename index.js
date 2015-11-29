8f8d86c6ee022ea327c70f56638dcee481c0920f
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-11-29 10:36:20 +0530

    export all the functions from the lib

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  accumulate: require('./lib/accumulate'),
+  chain: require('./lib/chain'),
+  combinations_with_replacement: require('./lib/combinations_with_replacement'),
+  combinations: require('./lib/combinations'),
+  compress: require('./lib/compress'),
+  count: require('./lib/count'),
+  cycle: require('./lib/cycle'),
+  dropwhile: require('./lib/dropwhile'),
+  filterfalse: require('./lib/filterfalse'),
+  groupby: require('./lib/groupby'),
+  islice: require('./lib/islice'),
+  permutations: require('./lib/permutations'),
+  product: require('./lib/product'),
+  repeat: require('./lib/repeat'),
+  starmap: require('./lib/starmap'),
+  takewhile: require('./lib/takewhile'),
+  tee: require('./lib/tee'),
+};
