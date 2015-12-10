f56327b01c9ce2d37ced0c7abf6f911b9c129875
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-12-10 15:23:07 +0530

    make the test names consistent

--- tests/accumulate/validations.js
+++ tests/accumulate/validations.js
@@ -1,20 +1,20 @@
 var expect = require('chai').expect;
 var accumulate = require('../../').accumulate;
 
-describe('when accumulate is passed invalid', function() {
-  it('function, it should throw an error', function() {
+describe('when accumulate is passed', function() {
+  it('an invalid function, it should throw an error', function() {
     expect(() => accumulate([], [])).to.throw(TypeError, /second argument to accumulate must be a function object/);
   });
 
-  it('iterable (undefined), it should throw an error', function() {
+  it('an invalid iterable (undefined), it should throw an error', function() {
     expect(() => accumulate()).to.throw(TypeError, /first argument to accumulate must be an iterable object/);
   });
 
-  it('iterable (null), it should throw an error', function() {
+  it('an invalid iterable (null), it should throw an error', function() {
     expect(() => accumulate()).to.throw(TypeError, /first argument to accumulate must be an iterable object/);
   });
 
-  it('iterable (number), it should throw an error', function() {
+  it('an invalid iterable (number), it should throw an error', function() {
     expect(() => accumulate(1)).to.throw(TypeError, /first argument to accumulate must be an iterable object/);
   });
 
