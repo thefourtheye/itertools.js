38c3481323cb3ec8d681f69911624979b41fee46
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-12-10 14:04:12 +0530

    throw if invalid iterable is passed to accumulate

--- tests/accumulate/validations.js
+++ tests/accumulate/validations.js
@@ -5,4 +5,17 @@ describe('when accumulate is passed invalid', function() {
   it('function, it should throw an error', function() {
     expect(() => accumulate([], [])).to.throw(TypeError, /second argument to accumulate must be a function object/);
   });
+
+  it('iterable (undefined), it should throw an error', function() {
+    expect(() => accumulate()).to.throw(TypeError, /first argument to accumulate must be an iterable object/);
+  });
+
+  it('iterable (null), it should throw an error', function() {
+    expect(() => accumulate()).to.throw(TypeError, /first argument to accumulate must be an iterable object/);
+  });
+
+  it('iterable (number), it should throw an error', function() {
+    expect(() => accumulate(1)).to.throw(TypeError, /first argument to accumulate must be an iterable object/);
+  });
+
 });
