dd6e093a4a9da0f313e9401daa36a60de9aac617
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-12-24 23:56:16 +0530

    add basic tests for chain.from_iterable

--- tests/chain.js
+++ tests/chain.js
@@ -25,3 +25,30 @@ describe('[Happy Cases] when chain is passed', function() {
   });
 
 });
+
+describe('[Failure Cases] when chain.from_iterable is passed', function() {
+
+  it('an invalid iterable, it should throw an error', function() {
+    expect(() => Array.from(chain.from_iterable(1))).to.throw(TypeError, 'not a valid iterable object [Number]');
+  });
+
+  it('no arguments, it should throw error as there are no valid iterables', function() {
+    expect(() => Array.from(chain.from_iterable())).to.throw(TypeError, 'not a valid iterable object [Undefined]');
+  });
+
+  it('a function, it should throw an error', function() {
+    expect(() => Array.from(chain.from_iterable(() => {})))
+      .to.throw(TypeError, 'not a valid iterable object [Function]');
+  });
+
+});
+
+describe('[Happy Cases] when chain.from_iterable is passed', function() {
+
+  it('valid iterables, it should return a valid chained iterable', function() {
+    expect(Array.from(chain.from_iterable([[1], [2, 3, 4]]))).to.be.deep.equal([1, 2, 3, 4]);
+    expect(Array.from(chain.from_iterable([[1], [2, 3, 4], [[5, 6, 7], [8, 9]]])))
+      .to.be.deep.equal([1, 2, 3, 4, [5, 6, 7], [8, 9]]);
+  });
+
+});
