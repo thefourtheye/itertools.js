479f7cb0a37d7d4e052d80d847493791ac1a203f
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-01-03 01:18:54 +0530

    fail if second parameter is negative for combinations

--- tests/combinations.js
+++ tests/combinations.js
@@ -1,10 +1,15 @@
 const expect = require('chai').expect;
 const combinations = require('../').combinations;
+const ValueError = require('../lib/utils').ValueError;
 
 describe('[Failure Cases] when combinations is passed', function() {
 
   it('an invalid integer, it should throw an error', function() {
     expect(() => combinations('abcd', 'abcd')).to.throw(TypeError, /abcd cannot be interpreted as an integer/);
+  });
+
+  it('a negative integer, it should throw an error', function() {
+    expect(() => combinations('abcd', -3)).to.throw(ValueError, /r must be non-negative/);
   });
 
 });
