2c462da1123d156a7cb1780b20d0060ab23ca328
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-01-05 00:05:23 +0530

    test basic cycle functionality

--- tests/cycle.js
+++ tests/cycle.js
@@ -1,6 +1,21 @@
 const expect = require('chai').expect;
 const cycle = require('../').cycle;
 
+function take(iterable, n) {
+  'use strict';
+  const result = [];
+  let counter = 0;
+  if (Number.isInteger(n) && n >= 0) {
+    for (const value of iterable) {
+      if (counter++ === n) {
+        break;
+      }
+      result.push(value);
+    }
+  }
+  return result;
+}
+
 describe('[Failure Cases] when cycle is passed', function() {
 
   it('no arguments, it should throw an error', function() {
@@ -13,6 +28,21 @@ describe('[Failure Cases] when cycle is passed', function() {
 
   it('an invalid iterable, it should throw an error', function() {
     expect(() => cycle(1)).to.throw(TypeError, '\'Number\' object is not iterable');
+  });
+
+});
+
+describe('[Happy Cases] when cycle is passed', function() {
+
+  it('an empty iterable, it should return an empty iterator', function() {
+    expect(Array.from(cycle([]))).to.deep.equal([]);
+  });
+
+  it('valid iterables', function() {
+    expect(take(cycle('abcd'), 5)).to.deep.equal(['a', 'b', 'c', 'd', 'a']);
+    expect(take(cycle('a'), 5)).to.deep.equal(['a', 'a', 'a', 'a', 'a']);
+    expect(take(cycle('abcd'), 3)).to.deep.equal(['a', 'b', 'c']);
+    expect(take(cycle('abcd'), 8)).to.deep.equal(['a', 'b', 'c', 'd', 'a', 'b', 'c', 'd']);
   });
 
 });
