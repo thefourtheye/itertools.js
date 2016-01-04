b5312478ac26e6cb61591d0bca4ff38f9feaaf95
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-01-04 23:45:10 +0530

    implement the basic count functionality

---
+++ tests/count.js
@@ -0,0 +1,73 @@
+const expect = require('chai').expect;
+const count = require('../').count;
+
+function take(iterable, n) {
+  'use strict';
+  const result = [];
+  let counter = 0;
+  if (Number.isInteger(n) && n >= 0) {
+    for (const value of iterable) {
+      if (counter++ === n) {
+        break;
+      }
+      result.push(value);
+    }
+  }
+  return result;
+}
+
+describe('[Failure Cases] when count is passed', function() {
+
+  it('an invalid number for first argument, it should throw an error', function() {
+    expect(() => count(null, 1)).to.throw(TypeError, /a number is required/);
+  });
+
+  it('an invalid number for second argument, it should throw an error', function() {
+    expect(() => count(1, null)).to.throw(TypeError, /a number is required/);
+  });
+
+  it('NaN as arguments, it should throw an error', function() {
+    expect(() => count(NaN, 1)).to.throw(TypeError, /a number is required/);
+    expect(() => count(1, NaN)).to.throw(TypeError, /a number is required/);
+  });
+
+  it('Infinity as arguments, it should throw an error', function() {
+    expect(() => count(Infinity, 1)).to.throw(TypeError, /a number is required/);
+    expect(() => count(1, Infinity)).to.throw(TypeError, /a number is required/);
+    expect(() => count(-Infinity, 1)).to.throw(TypeError, /a number is required/);
+    expect(() => count(1, -Infinity)).to.throw(TypeError, /a number is required/);
+  });
+
+  it('an invalid number for second argument, it should throw an error', function() {
+    expect(() => count(1, null)).to.throw(TypeError, /a number is required/);
+  });
+
+  it('greater than two arguments, it should throw an error', function() {
+    expect(() => count([], [], [])).to.throw(TypeError, 'count() takes at most 2 arguments (3 given)');
+  });
+
+});
+
+describe('[Happy Cases] when count is passed', function() {
+
+  it('no start and step values, the default values 0 and 1 should be assumed respectively', function() {
+    expect(take(count(), 10)).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it('no step value, the default value 1 should be assumed', function() {
+    expect(take(count(1), 5)).to.deep.equal([1, 2, 3, 4, 5]);
+  });
+
+  it('both valid start and step value as integers', function() {
+    expect(take(count(1, 5), 5)).to.deep.equal([1, 6, 11, 16, 21]);
+  });
+
+  it('step as a negative integer', function() {
+    expect(take(count(1, -1), 5)).to.deep.equal([1, 0, -1, -2, -3]);
+  });
+
+  it('both start and step as a floating point values', function() {
+    expect(take(count(0.5, 0.25), 5)).to.deep.equal([0.5, 0.75, 1.0, 1.25, 1.5]);
+  });
+
+});
